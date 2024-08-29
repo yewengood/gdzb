0a07c7191fabf5cb77e8a2520ffb2c417c87979d
yewengood <152235769+yewengood@users.noreply.github.com>
2024-08-29 22:34:42 +0800

    Update fofa.sh

--- speedtest/fofa.sh
+++ speedtest/fofa.sh
@@ -58,7 +58,7 @@ case $city_choice in
         city="Guangdong_103"
         stream="udp/239.20.0.101:2000"
 	channel_key="广东移动"
-        url_fofa=$(echo  '"udpxy" && country="CN" && region="Guangdong" && org="China Mobile Communications Group Co., Ltd."' | base64 |tr -d '\n')
+        url_fofa=$(echo  '"udpxy" && country="CN" && region="Guangdong" && protocol="http"' | base64 |tr -d '\n')
         url_fofa="https://fofa.info/result?qbase64="$url_fofa
         ;;
     3)
