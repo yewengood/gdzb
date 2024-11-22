e467ed52d5bc601bf80097705b41dcb5fad2201e
yewengood <152235769+yewengood@users.noreply.github.com>
2024-11-22 16:42:09 +0800

    Update tv.sh

--- speedtest/tv.sh
+++ speedtest/tv.sh
@@ -33,15 +33,14 @@ case $city_choice in
         city="Sichuan"
         stream="udp/239.93.42.33:5140"
         channel_key="四川电信"
-        url_fofa=$(echo  '"udpxy" && country="CN" && region="Sichuan" && org="CHINA UNICOM China169 Backbone"  && protocol="http"' | base64 |tr -d '\n')
-        url_fofa=$(echo  '"udpxy" && country="CN" && region="Sichuan" && protocol="http"' | base64 |tr -d '\n')
+        url_fofa=$(echo  '"udpxy" && country="CN" && region="Sichuan"' | base64 |tr -d '\n')
         url_fofa="https://fofa.info/result?qbase64="$url_fofa
         ;;
     2)
         city="Beijing"
         stream="rtp/239.3.1.129:8008"
         channel_key="北京联通"
-        url_fofa=$(echo  '"udpxy" && country="CN" && region="Beijing" && org="China Unicom Beijing Province Network" && protocol="http"' | base64 |tr -d '\n')
+        url_fofa=$(echo  '"udpxy" && country="CN" && region="Beijing"' | base64 |tr -d '\n')
         url_fofa="https://fofa.info/result?qbase64="$url_fofa
         ;;
    
