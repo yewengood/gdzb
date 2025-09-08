bb8b401d34fdb310aa0b061612bbaed0adb2bb6e
yewengood <152235769+yewengood@users.noreply.github.com>
2025-09-08 17:41:49 +0800

    Update dx.sh

--- speedtest/dx.sh
+++ speedtest/dx.sh
@@ -33,7 +33,7 @@ time=$(date +%Y%m%d%H%M%S) # 定义 time 变量
 while IFS= read -r line; do
     i=$((i + 1))
     ip="$line"
-    url="http://$ip/udp/239.77.0.114:5146"
+    url="http://$ip/udp/239.93.0.252:5140"
     echo "$url"
     curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
     a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
@@ -54,16 +54,10 @@ rm -f "speedtest_${city}_$time.log"
 program="template/template_${city}.txt"
 
 sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
-sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
-sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
-cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
-rm -rf tmp1.txt tmp2.txt tmp3.txt
+cat tmp1.txt > "txt/fofa_${city}.txt"
+rm -rf tmp1.txt
 rm -rf gddx_fofa.txt
 #--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
 echo "📡  电信广电,#genre#" >>gddx_fofa.txt
 cat txt/fofa_dianxin.txt >>gddx_fofa.txt
-echo "📡  四川频道,#genre#" >>gddx_fofa.txt
-cat txt/fofa_Sichuan.txt >>gddx_fofa.txt
-echo "📡  北京联通,#genre#" >>gddx_fofa.txt
-cat txt/fofa_Beijing.txt >>gddx_fofa.txt
 for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
